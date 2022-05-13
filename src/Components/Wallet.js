aef2ede384c82dc5ce481c44c7d55158ff62d9c1
yudiz_nisha <nisha.s@yudiz.com>
2022-05-13 19:56:31 +0530

    Error solved

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -1,12 +1,20 @@
 import React, { Fragment, useEffect } from 'react'
 // import PropTypes from 'prop-types'
+import Web3 from 'web3'
 
 const Wallet = props => {
 
   const handleWallet = async () => {
-    const provider = window.ethereum
-    const account = await provider.request({ method: 'eth_requestAccounts'})
-    console.log('account', account)
+    const web3 = new Web3(Web3.givenProvider);
+    console.log('web3', web3)
+    // const provider = window.ethereum
+    // const account = await provider.request({ method: 'eth_requestAccounts'})
+    // const accounts = await provider.request({ method: 'eth_getAccounts'})
+
+    const accounts = await web3.eth.requestAccounts();
+
+    // console.log('account', account)
+    console.log('accounts', accounts)
   }
   
   useEffect(() => {
